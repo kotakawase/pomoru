763289380526054a23be4e7a6bff955f1383ebe8
kotakawase <kottan00@gmail.com>
2022-02-18 16:07:06 +0900

    コマンドをmodule化

---
+++ bot/bot.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'discordrb'
+require 'dotenv/load'
+
+module Bot
+  TOKEN = ENV['TOKEN']
+  CLIENT_ID = ENV['CLIENT_ID']
+  PREFIX = ENV['PREFIX']
+  BOT = Discordrb::Commands::CommandBot.new(
+      token: TOKEN,
+      client_id: CLIENT_ID,
+      prefix: PREFIX,
+      help_command: false,
+  )
+
+  def self.load_modules(cls, path)
+    new_module = Module.new
+    const_set(cls.to_sym, new_module)
+    Dir["bot/modules/#{path}/*rb"].each { |file| load file }
+    new_module.constants.each do |mod|
+      BOT.include! new_module.const_get(mod)
+    end
+  end
+
+  load_modules(:Commands, 'commands')
+  load_modules(:Events, 'events')
+
+  BOT.run :async
+  puts "Bot is running!"
+  BOT.sync
+end
