fa27656b3c853cc5ae1cc96e23f8ca01b50dbe37
kotakawase <kottan00@gmail.com>
2022-02-19 11:34:03 +0900

    botを呼び出せるように修正

--- bot/modules/commands/control.rb
+++ bot/modules/commands/control.rb
@@ -11,7 +11,7 @@ module Bot::Commands
       if channel.nil?
         event.send_message('ボイスチャンネルに接続できません。接続するには[pmt!start]コマンドを入力するユーザーがボイスチャンネルに参加している必要があります。')
       else
-        bot.voice_connect(channel)
+        event.bot.voice_connect(channel)
         event.send_message("#{channel.name} に参加しました。")
         # event.voice.play_file("#{Dir.pwd}/sounds/sounds_pomo_start.mp3")
       end
@@ -32,7 +32,7 @@ module Bot::Commands
     command :end do |event|
       channel = event.user.voice_channel
       if channel
-        bot.voice_destroy(event.server.id)
+        event.bot.voice_destroy(event.server.id)
         next 'Good Bye!'
       else
         event.send_message('Can not disconnect')
