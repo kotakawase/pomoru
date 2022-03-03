5e56d359d919c7c80223523a126f39143f49b2bb
kotakawase <kottan00@gmail.com>
2022-03-03 10:35:29 +0900

    pomodoroフォルダの削除

--- lib/bot/pomodoro/break.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require_relative './timer_base'
-
-class Break < TimerBase
-end
--- lib/bot/pomodoro/short_break.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require_relative './break'
-
-class ShortBreak < Break
-end
