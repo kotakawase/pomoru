a1cf0c6a113dc69bd09c22449cb868e913ed6a2d
kotakawase <kottan00@gmail.com>
2022-03-24 00:50:19 +0900

    テストコードの作成

---
+++ test/message_builder_test.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require 'discordrb'
+require 'minitest/autorun'
+require_relative '../lib/pomoru/message_builder'
+require_relative '../lib/pomoru/session/session'
+require_relative '../lib/pomoru/settings'
+require_relative '../lib/pomoru/state'
+require_relative '../lib/pomoru/stats'
+
+class MessageBuilderTest < Minitest::Test
+  def setup
+    pomodoro = 25
+    short_break = 5
+    long_break = 15
+    intervals = 4
+    @session = Session.new(
+      state: State::POMODORO,
+      set: Settings.new(
+        pomodoro,
+        short_break,
+        long_break,
+        intervals
+      )
+    )
+  end
+
+  def test_status_embed
+    title = 'Timer'
+    status_str = "Current intervals: Pomodoro\n \
+        Status: Pausing\n \
+        Reminder alerts: Off\n \
+        Autoshush: Off"
+
+    assert_equal(title, MessageBuilder.status_embed(@session).title)
+    assert_equal(status_str, MessageBuilder.status_embed(@session).description)
+  end
+
+  def test_settings_embed
+    title = 'Session settings'
+    settings_str = "Pomodoro: 25 min\n \
+        Short break: 5 min\n \
+        Long break: 15 min\n \
+        Interbals: 4"
+
+    assert_equal(title, MessageBuilder.settings_embed(@session).title)
+    assert_equal(settings_str, MessageBuilder.settings_embed(@session).description)
+  end
+
+  def test_stats_embed
+    stats_msg = 'You completed 0 pomodoro (0minutes)'
+    assert_equal(stats_msg, MessageBuilder.stats_msg(@session.stats))
+  end
+
+  def test_no_arguments_passed_to_help_embed
+    summary = <<~TEXT
+      pomoru is a Discord bot that allows you to use the Pomodoro technique on Discord.
+      Set periods of focus to get work done and chat during the breaks.
+
+      Required parameters are enclosed in <> and optional parameters are enclosed in [].
+      For example, you can do "pmt!start" to start a pomodoro session with the default values or "pmt!start 30 10" to customize the pomodoro and short break durations!
+    TEXT
+
+    assert_equal('Help menu', MessageBuilder.help_embed(nil).title)
+    assert_equal(summary, MessageBuilder.help_embed(nil).description)
+  end
+
+  def test_command_is_passed_to_help_embed
+    start = <<~TEXT
+      Start pomodoro session with optional custom settings.
+
+      pomodoro: duration of each pomodoro interval in minutes (default: 25 min)
+      short_break: duration of short breaks in minutes (default: 5 min)
+      long_break: duration of long breaks in minutes (default: 15 min)
+      intervals: number of pomodoro intervals between each long break (default: 4)
+    TEXT
+
+    assert_equal('start [pomodoro] [short_break] [long_break] [intervales]', MessageBuilder.help_embed('start').title)
+    assert_equal(start, MessageBuilder.help_embed('start').description)
+  end
+
+  def test_reminders_embed
+    title = 'Reminder alerts'
+    reminders_str = "Pomodoro: 5 min\n \
+        Short break: 1 min\n \
+        Long break: 5 min"
+
+    assert_equal(title, MessageBuilder.reminders_embed(@session).title)
+    assert_equal(reminders_str, MessageBuilder.reminders_embed(@session).description)
+  end
+end
---
+++ test/session_controller_test.rb
@@ -0,0 +1,99 @@
+require 'minitest/autorun'
+require_relative '../lib/pomoru/session/reminder'
+require_relative '../lib/pomoru/session/session_controller'
+require_relative '../lib/pomoru/session/session'
+require_relative '../lib/pomoru/settings'
+require_relative '../lib/pomoru/state'
+
+class SessionControllerTest < Minitest::Test
+  def setup
+    pomodoro = 25
+    short_break = 5
+    long_break = 15
+    intervals = 4
+    @session = Session.new(
+      state: State::POMODORO,
+      set: Settings.new(
+        pomodoro,
+        short_break,
+        long_break,
+        intervals
+      )
+    )
+  end
+
+  # edit
+  def test_pomodoro_timer_settings_before_editing
+    assert_equal(25, @session.settings.pomodoro)
+    assert_equal(5, @session.settings.short_break)
+    assert_equal(15, @session.settings.long_break)
+    assert_equal(4, @session.settings.intervals)
+  end
+
+  def test_pomodoro_timer_settings_after_editing
+    SessionController.edit(@session, Settings.new(10, 5, 8, 3))
+    assert_equal(10, @session.settings.pomodoro)
+    assert_equal(5, @session.settings.short_break)
+    assert_equal(8, @session.settings.long_break)
+    assert_equal(3, @session.settings.intervals)
+  end
+
+  def test_set_the_pomodoro_timer_to_a_higher_value_when_the_reminder_is_on
+    @session.reminder.running = true
+    SessionController.edit(@session, Settings.new(6, 2, 6))
+    assert_equal(5, @session.reminder.pomodoro)
+    assert_equal(1, @session.reminder.short_break)
+    assert_equal(5, @session.reminder.long_break)
+  end
+
+  def test_set_the_pomodoro_timer_to_the_same_value_when_reminders_are_on
+    @session.reminder.running = true
+    SessionController.edit(@session, Settings.new(5, 2, 5))
+    assert_equal(1, @session.reminder.pomodoro)
+    assert_equal(1, @session.reminder.short_break)
+    assert_equal(1, @session.reminder.long_break)
+  end
+
+  def test_set_the_pomodoro_timer_to_a_lower_value_when_the_reminder_is_on
+    @session.reminder.running = true
+    SessionController.edit(@session, Settings.new(4, 1, 4))
+    assert_equal("None", @session.reminder.pomodoro)
+    assert_equal("None", @session.reminder.short_break)
+    assert_equal("None", @session.reminder.long_break)
+  end
+
+  # remind
+  def test_reminder_settings_before_editing
+    assert_equal(5, @session.reminder.pomodoro)
+    assert_equal(1, @session.reminder.short_break)
+    assert_equal(5, @session.reminder.long_break)
+  end
+
+  def test_all_reminder_settings_are_more_than_pomodoro_timer
+    SessionController.remind(@session, Reminder.new(26, 6, 16))
+    assert_equal(1, @session.reminder.pomodoro)
+    assert_equal(1, @session.reminder.short_break)
+    assert_equal(1, @session.reminder.long_break)
+  end
+
+  def test_all_reminder_settings_are_same_as_the_pomodoro_timer
+    SessionController.remind(@session, Reminder.new(25, 5, 15))
+    assert_equal(1, @session.reminder.pomodoro)
+    assert_equal(1, @session.reminder.short_break)
+    assert_equal(1, @session.reminder.long_break)
+  end
+
+  def test_reminder_settings_after_editing
+    SessionController.remind(@session, Reminder.new(24, 4, 14))
+    assert_equal(24, @session.reminder.pomodoro)
+    assert_equal(4, @session.reminder.short_break)
+    assert_equal(14, @session.reminder.long_break)
+  end
+
+  def test_all_reminder_settings_are_0
+    SessionController.remind(@session, Reminder.new(0, 0, 0))
+    assert_equal("None", @session.reminder.pomodoro)
+    assert_equal("None", @session.reminder.short_break)
+    assert_equal("None", @session.reminder.long_break)
+  end
+end
