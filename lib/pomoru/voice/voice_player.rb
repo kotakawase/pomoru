be39b8f9cd983cb29e72cd5b1508d31d18d891f0
kotakawase <kottan00@gmail.com>
2022-04-01 11:35:47 +0900

    セッションに応じて適切なアラートが再生されるよう修正

--- lib/pomoru/voice/voice_player.rb
+++ lib/pomoru/voice/voice_player.rb
@@ -5,21 +5,23 @@ require_relative '../state'
 class VoicePlayer
   POMO_START = "#{Dir.pwd}/sounds/pomodoro_start.mp3".freeze
   POMO_END = "#{Dir.pwd}/sounds/pomodoro_end.mp3".freeze
-  REMIND_START = "#{Dir.pwd}/sounds/remind_start.mp3".freeze
+  REMIND_ALERT = "#{Dir.pwd}/sounds/remind_start.mp3".freeze
 
   class << self
     def alert(session, value = nil)
       return unless session.event.voice
 
-      if value.nil? || (session.state == State::SHORT_BREAK || session.state == State::LONG_BREAK)
-        path = POMO_START # timer start and pomodoro
-      elsif value == session.timer.end
+      if value.nil? || (value == session.timer.end && !(session.state == State::POMODORO))
+        path = POMO_START # start and pomodoro
+      elsif value == session.timer.end && session.state == State::POMODORO
         path = POMO_END # short break and long break
       elsif value == session.reminder.end
-        path = REMIND_START # remind
+        path = REMIND_ALERT # remind
       end
       init_alert_thread(session, path)
     end
+
+    private
 
     def init_alert_thread(session, path)
       Thread.new { session.event.voice.play_file(path) }
